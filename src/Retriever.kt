b8c072fcf75f67128dbdebff74772920a403e154
Jason Holmes <holmes@squareup.com>
2017-02-12 08:04:45 -0800

    All the data

---
+++ src/Retriever.kt
@@ -0,0 +1,36 @@
+import java.io.BufferedReader
+import java.io.FileOutputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.URL
+import java.time.LocalDate
+
+fun main(args: Array<String>) {
+  val start = LocalDate.of(2031, 1, 1)
+  val end = LocalDate.of(2051, 1, 1)
+
+  val lat = "37.8857582"
+  val lon = "-122.1180201"
+
+  var currentDate = start
+
+  while (currentDate < end) {
+    val year = currentDate.year
+    val month = currentDate.monthValue
+    val day = currentDate.dayOfMonth
+
+    val targetURL = "http://api.sunrise-sunset.org/json?lat=$lat&lng=$lon&date=$year-$month-$day&formatted=0"
+    val file = OutputStreamWriter(FileOutputStream("$year-$month-$day.json"))
+
+    val connection = URL(targetURL).openConnection()
+    val reader = BufferedReader(InputStreamReader(connection.getInputStream()))
+
+    // It's 1 line. I don't care.
+    file.write(reader.readLine())
+    reader.close()
+    file.close()
+
+    currentDate = currentDate.plusDays(1)
+    Thread.sleep(1000)
+  }
+}
